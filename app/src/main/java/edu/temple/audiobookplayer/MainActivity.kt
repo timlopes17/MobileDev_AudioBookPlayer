524e5127a749476287ed0f970be9126c7385300c
timlopes17 <timlopes0817@gmail.com>
2022-03-31 00:43:37 -0400

    FINALLY found a way to get the query of the search with a single activitiy

--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -10,6 +10,11 @@ import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import edu.temple.audiobookplayer.Book
 import edu.temple.audiobookplayer.BookList
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class MainActivity : AppCompatActivity(), BookListFragment.SelectionFragmentInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +24,13 @@ class MainActivity : AppCompatActivity(), BookListFragment.SelectionFragmentInte
         val bookList = BookList()
         val searchButton = findViewById<Button>(R.id.searchButton)
 
+        lateinit var bookViewModel: BookViewModel
+        bookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
+
         searchButton.setOnClickListener {
             onSearchRequested()
         }
 
-        lateinit var bookViewModel: BookViewModel
-        bookViewModel = ViewModelProvider(this).get(BookViewModel::class.java)
-
-        for (i in 0..9){
-            //bookList.add(Book(titleArray[i], authorArray[i]))
-        }
 
         var fragment = supportFragmentManager.findFragmentById(R.id.container1)
         if(fragment != null){
@@ -55,6 +57,30 @@ class MainActivity : AppCompatActivity(), BookListFragment.SelectionFragmentInte
                 .replace(R.id.container1, BookDetailsFragment())
                 .addToBackStack(null)
                 .commit()
+        }
+    }
+
+    suspend fun searchBooks(search : String){
+        Log.d("Server Response", "Fuck me")
+        withContext(Dispatchers.IO) {
+            val serverResponse = URL("https://kamorris.com/lab/cis3515/search.php")
+                .openStream()
+                .bufferedReader()
+                .readLine()
+
+            Log.d("Server Response", serverResponse)
+        }
+
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (Intent.ACTION_SEARCH == intent!!.action) {
+            intent!!.getStringExtra(SearchManager.QUERY)?.also { query ->
+                CoroutineScope(Dispatchers.Main).launch() {
+                    searchBooks(query)
+                }
+            }
         }
     }
 }
